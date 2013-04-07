150fa4587faf831f782acc71153759138abe7596
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2013-04-06 21:29:50 -0400

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,8 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true, length: { :maximum => 140 }
+
+  belongs_to :user
+
+ 	validates :user_id, presence: true
 end
